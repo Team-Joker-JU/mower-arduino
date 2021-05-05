d2d626fc7115ac22f225c0853f15949d3b1fe41c
Warcaith <f.v.carlsson@gmail.com>
2021-05-05 22:47:29 +0200

    feat: add enum for 'robotcommand'

---
+++ src/robotcommand.h
@@ -0,0 +1,17 @@
+#ifndef ROBOTCOMMAND_H
+#define ROBOTCOMMAND_H
+
+#include <stdint.h>
+
+enum RobotCommand : int8_t
+{
+  HANDSHAKE = 0,
+  CONNECTED = 1,
+  RECIEVED  = 2,
+  ACCELERATION = 3,
+  STEERING = 4,
+  COLLISION = 5
+};
+  
+
+#endif
